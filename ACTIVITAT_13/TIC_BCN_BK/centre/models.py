4c67366215431899a1cabf6028e3f176a6f69c1c
Brandy <2023_brandy.sherile@iticbcn.cat>
2025-01-22 20:51:32 +0100

    files updated

--- ACTIVITAT_13/TIC_BCN_BK/centre/models.py
+++ ACTIVITAT_13/TIC_BCN_BK/centre/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Students(models.Model):
+    nom = models.CharField(max_length=20)
+    cognom1 = models.CharField(max_length=20)
+    cognom2 = models.CharField(max_length=20)
+    correu = models.CharField(max_length=50)
+    curs = models.CharField(max_length=10)
+    moduls_matriculats = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nom} {self.cognom1} {self.cognom2}"
+
+
+class Teachers(models.Model):
+    id = models.IntegerField(max_length=10, primary_key=True)
+    nom = models.CharField(max_length=20)
+    cognom = models.CharField(max_length=20)
+    edat = models.IntegerField(max_length=20)
+    rol = models.CharField(max_length=20)
+    curs = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nom} {self.cognom} {self.curs}"
+
--- ACTIVITAT_13/TIC_BCN_BK/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_BK/centre/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Students, Teachers
+
 
 # Create your views here.
+
+
+def students(request):
+    students = Students.objects.all()
+    return render(request, 'templates/students.html', {'students': students})
+
+
+def teachers(request):
+    teachers = Teachers.objects.all()
+    return render(request, 'templates/teachers.html', {'teachers': teachers})
